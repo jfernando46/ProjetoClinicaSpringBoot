77db25b3496102412977944b3ffd19f81afee8eb
Jose Fernando <jfernandofenix.fs@gmail.com>
2021-10-20 08:53:12 -0300

    Inicio do Projeto Clinica API

---
+++ src/test/java/br/com/jose/clinicaapi/ClinicaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jose.clinicaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
